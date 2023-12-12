854e5eb3e3f58825f9f45b16745c19e26fd8c850
Jhonatan Pereira <jhonatangopereira@gmail.com>
2023-12-12 00:07:10 -0300

    add mqtt client implementation in esp

--- esp/main/main.c
+++ esp/main/main.c
@@ -15,6 +15,7 @@
 #include "sdkconfig.h"
 #include "driver/gpio.h"
 #include <time.h>
+#include "mqtt_client.h"
 
 char *TAG = "BLE-Server";
 uint8_t ble_addr_type;
@@ -24,11 +25,43 @@ void ble_app_advertise(void);
 #define ledG 12
 char tolken[50];
 
+// MQTT Configurações
+#define MQTT_SERVER "192.168.7.2:1883"
+#define MQTT_TOPIC_REQUEST "token/access"
+#define MQTT_TOPIC_RESPONSE "token/response"
+
+// Função para enviar mensagem MQTT
+void enviarMensagemMQTT(const char *topic, const char *message) {
+    // Inicialize o cliente MQTT
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    MQTTClient_message pubmsg = MQTTClient_message_initializer;
+    MQTTClient_deliveryToken token;
+
+    // Conecte-se ao servidor MQTT
+    MQTTClient_create(&client, MQTT_SERVER, "ClientID", MQTTCLIENT_PERSISTENCE_NONE, NULL);
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+
+    if (MQTTClient_connect(client, &conn_opts) != MQTTCLIENT_SUCCESS) {
+        printf("Falha ao conectar ao servidor MQTT\n");
+        return;
+    }
+
+    // Envie a mensagem para o tópico MQTT
+    pubmsg.payload = message;
+    pubmsg.payloadlen = strlen(message);
+    pubmsg.qos = 1;
+    pubmsg.retained = 0;
+    MQTTClient_publishMessage(client, topic, &pubmsg, &token);
+
+    MQTTClient_disconnect(client, 10000);
+    MQTTClient_destroy(&client);
+}
 
 // Write data to ESP32 defined as server
 static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
 {
-    
     char * data = (char *)ctxt->om->om_data;
     char textoRec = data;
     uint16_t * tam = ctxt->om->om_len;
@@ -39,34 +72,9 @@ static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_g
     }
     printf("%s\n", tolken);
     printf("\n");
-    
-    char* string_desejada = "ligar"; // String a ser comparada
-    char string_mateus[] = "067d014b242d2945";
-    char string_eduarda[] = "e5def65a3d13ac70";
-    
-    if (strcmp(tolken,string_eduarda) == 0) { 
-        
-        printf("token permitido");
-        gpio_set_level(Rele, 0);
-        gpio_set_level(ledG, 1);
 
-        vTaskDelay(100);
-        gpio_set_level(Rele, 1);
-        gpio_set_level(ledG, 0);
-        
-    }
-
-    if (strcmp(tolken,string_mateus) == 0) { 
-        
-        printf("token permitido");
-        gpio_set_level(Rele, 0);
-        gpio_set_level(ledG, 1);
-
-        vTaskDelay(100);
-        gpio_set_level(Rele, 1);
-        gpio_set_level(ledG, 0);
-        
-    }
+    // Subscrever ao tópico de resposta MQTT
+    enviarMensagemMQTT(MQTT_TOPIC_REQUEST, tolken);
 
     memset(tolken, 0, sizeof(tolken));
     
@@ -185,8 +193,78 @@ static void init_led(void)
     gpio_set_direction(ledG, GPIO_MODE_OUTPUT);
 }
 
+// void app_main()
+// {   
+//     init_led();
+//     connectBLE();
+// }
+
+
+// Função para tratar mensagens MQTT recebidas
+int mensagemMQTTCallback(void* context, char* topicName, int topicLen, MQTTClient_message* message) {
+    printf("Mensagem recebida no tópico %s: %.s\n", topicName, message->payloadlen, (char)message->payload);
+
+    // Processar a mensagem recebida e acionar/desacionar o gpio_set_level conforme necessário
+    if (strcmp((char*)message->payload, "ok") == 0) {
+        gpio_set_level(Rele, 0);
+        gpio_set_level(ledG, 1);
+
+        vTaskDelay(100);
+        gpio_set_level(Rele, 1);
+        gpio_set_level(ledG, 0);
+    } else {
+        gpio_set_level(Rele, 1);
+        gpio_set_level(ledG, 0);
+    }
+
+    MQTTClient_freeMessage(&message);
+    MQTTClient_free(topicName);
+
+    return 1;
+}
+
+// Write data to ESP32 defined as server
+static int device_write(uint16_t conn_handle, uint16_t attr_handle, struct ble_gatt_access_ctxt *ctxt, void *arg)
+{
+    gpio_set_level(ledR, 1);
+    char * data = (char *)ctxt->om->om_data;
+    char textoRec = data;
+    uint16_t * tam = ctxt->om->om_len;
+
+    printf("Data from the client: %.*s\n", ctxt->om->om_len, ctxt->om->om_data);
+    for(int i = 0; i < ctxt->om->om_len; i++){
+        tolken[i] = ctxt->om->om_data[i];
+    }
+    printf("%s\n", tolken);
+    printf("\n");
+
+    // Envie o token como requisição para a comparação MQTT
+    enviarMensagemMQTT(MQTT_TOPIC_REQUEST, tolken);
+
+    memset(tolken, 0, sizeof(tolken));
+
+    return 0;
+}
+
+// ... (restante do código)
+
 void app_main()
 {   
     init_led();
     connectBLE();
+
+    // Configurar MQTT
+    MQTTClient client;
+    MQTTClient_connectOptions conn_opts = MQTTClient_connectOptions_initializer;
+    conn_opts.keepAliveInterval = 20;
+    conn_opts.cleansession = 1;
+    MQTTClient_create(&client, MQTT_SERVER, "ClientID", MQTTCLIENT_PERSISTENCE_NONE, NULL);
+
+    while (1) {
+        // Lidar com mensagens MQTT recebidas
+        MQTTClient_message message;
+        if (MQTTClient_receive(client, &message, 1000) == MQTTCLIENT_SUCCESS) {
+            mensagemMQTTCallback(NULL, MQTT_TOPIC_RESPONSE, strlen(MQTT_TOPIC_RESPONSE), &message);
+        }
+    }
 }
